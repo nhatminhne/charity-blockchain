b05d68d874f35f10abdf3f7df1ccfe9df5d555f5
nhatminhmibo <minhnguyen2259@gmail.com>
2022-05-20 17:08:37 +0700

    add next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,34 @@ module.exports = {
   future: {
     webpack5: false,
   },
+
+  async headers() {
+    return [
+      {
+        source: '/campaign/new', // becomes /docs/with-basePath
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          },
+        ],
+      }
+    ]
+  },
 };
